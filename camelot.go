55996c7b74b02c8a5e7c6544f26c5da64f5becdd
mrfelfel <felfelpardaz@gmail.com>
2018-07-22 05:47:56 +0430

    add Files

---
+++ camelot.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yaa110/go-persian-calendar/ptime"
+)
+
+type Taqvim struct {
+}
+
+func main() {
+	mah := [6]string{"Odin", "Thor", "Loki", "hel", "Menglad", "surtr"}
+	week := [9]string{"Asgard", "Alfheim", "Vanaheim", "Midgard", "Jotunheim", "Svartalfaheim", "Muspelheim", "Niflheim", "Helheim"}
+	gmo := GetMonth()
+	weekr := ""
+	if gmo[0] < 3 {
+		weekr += week[gmo[1]/8-gmo[1]/9-1]
+	} else {
+		// fmt.Println(gmo[1] / 6)
+	}
+	fmt.Println(mah[gmo[0]-1], gmo[1], weekr)
+}
+func GetMonth() [2]int {
+
+	pt := ptime.Now(ptime.Iran())
+
+	if pt.YearDay() < 144 {
+		if pt.YearDay() <= 72 {
+			return [2]int{1, pt.YearDay()}
+		} else {
+			return [2]int{2, pt.YearDay() - 72}
+
+		}
+	} else {
+		mah := 4/(pt.RYearDay()/54) + 1
+		return [2]int{mah + 2, pt.RYearDay() - mah*54 - 5}
+	}
+}
